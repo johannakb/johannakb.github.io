7aea8def62ef345eb5e53920e396f3367c76e91d
johannakb <johannakb@gmail.com>
2019-09-19 21:18:11 +0000

    Template stuff

--- src/components/photo.js
+++ src/components/photo.js
@@ -1,12 +1,12 @@
 import React, {Component} from 'react';
-//import Jojo from '../jojo.jpg'
+import Jojo from '../jojo.jpg'
 //import '../css/photos.css';
 
 class photo extends Component {
     render(){
         return(
             <div>
-                <img src = "/Users/johannakb/Documents/Forrit/portfolio-app/public/jojo.jpg" alt="Mynd af mér" />
+                <img src = "Jojo" alt="Mynd af mér" />
             </div>
         )
     }
