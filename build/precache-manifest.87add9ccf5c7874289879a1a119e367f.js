bd5c924e24d05999d2651a0ff8b8292195d42aa0
johannakb <johannakb@gmail.com>
2019-09-19 17:43:13 +0000

    testing 3

---
+++ build/precache-manifest.87add9ccf5c7874289879a1a119e367f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adfb0831b435b1e9f1278021accd1a41",
+    "url": "/myPortfolio/index.html"
+  },
+  {
+    "revision": "d6e998db774aed87da65",
+    "url": "/myPortfolio/static/css/main.21cbb792.chunk.css"
+  },
+  {
+    "revision": "7cd55ef600c2713d3a16",
+    "url": "/myPortfolio/static/js/2.42ae9150.chunk.js"
+  },
+  {
+    "revision": "d6e998db774aed87da65",
+    "url": "/myPortfolio/static/js/main.df9756b6.chunk.js"
+  },
+  {
+    "revision": "2261391d3370eb3cf5eb",
+    "url": "/myPortfolio/static/js/runtime~main.918aa22c.js"
+  }
+]);
