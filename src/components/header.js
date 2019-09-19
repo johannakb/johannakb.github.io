8a62c1b05c85886f7bb2fb80f4d203362e7e5d3f
johannakb <johannakb@gmail.com>
2019-09-19 20:54:44 +0000

    setja header

---
+++ src/components/header.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import '../css/header.css';
+//import '../css/photos.css';
+
+class header extends Component {
+    render(){
+        return(
+            <div>
+                <h1>
+                    Um okkur
+                </h1>
+            </div>
+        )
+    }
+
+}
+export default header;
